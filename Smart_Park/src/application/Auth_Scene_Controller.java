74321d134bcd152e30b4a7c51351aef6c53bba8b
zulu-golf <zikkoyaya@gmail.com>
2020-01-20 16:33:55 +0000

    meme que la derniere

---
+++ Smart_Park/src/application/Auth_Scene_Controller.java
@@ -0,0 +1,27 @@
+package application;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+public class Auth_Scene_Controller implements Initializable{
+
+	public Auth_Scene_Controller() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		// TODO Auto-generated method stub
+		
+	}
+	@FXML
+	private void exitProgramAction(ActionEvent exitProgramEvent) {        
+	    System.out.println("Killing program...");
+	    System.exit(0);
+	}
+
+}
---
+++ Smart_Park/src/application/Enr_Scene_Controller.java
@@ -0,0 +1,20 @@
+package application;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.Initializable;
+
+public class Enr_Scene_Controller implements Initializable {
+
+	public Enr_Scene_Controller() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
