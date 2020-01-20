55efcdfbdcd494e234d3e098f0284e596facb60c
zulu-golf <zikkoyaya@gmail.com>
2020-01-20 16:33:26 +0000

    l'ajout de seux scenes de l'authentification et enregistremets
    aussi les classes controlleur corespondant

--- Smart_Park/src/application/Main.java
+++ Smart_Park/src/application/Main.java
@@ -1,26 +1,62 @@
 package application;
 	
 import javafx.application.Application;
+import javafx.event.Event;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.paint.Color;
 
 
 public class Main extends Application {
+	private double xOffset=0;
+	private double yOffset=0;
 	@Override
-	public void start(Stage primaryStage) {
-		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
-	}
-	
-	public static void main(String[] args) {
-		launch(args);
-	}
+	    public void start(Stage primaryStage) {
+	        try {
+	      
+	            // Read file fxml and draw interface.
+	           Parent auth_scene = FXMLLoader.load(getClass()
+	                    .getResource("/application/Auth_Scene.fxml"));
+	          /* auth_scene.setOnMousePressed(new EventHandler<MouseEvent>   () {
+
+				@Override
+				public void handle(MouseEvent event) {
+					// TODO Auto-generated method stub
+					xOffset= event.getSceneX();
+					yOffset= event.getSceneY();
+					
+				}
+			});
+	           auth_scene.setOnMouseDragged(new EventHandler<MouseEvent>() {
+
+				@Override
+				public void handle(MouseEvent event) {
+					// TODO Auto-generated method stub
+					primaryStage.setX(event.getSceneX() - xOffset);
+
+					primaryStage.setY(event.getSceneY() - yOffset);
+					
+				}
+			});*/
+	           primaryStage.initStyle(StageStyle.TRANSPARENT);
+	            primaryStage.setTitle("Bibliotheque");
+	            Scene scene=new Scene(auth_scene);
+	            scene.setFill(Color.TRANSPARENT);
+	            primaryStage.setScene(scene);
+	            primaryStage.show();
+	         
+	        } catch(Exception e) {
+	            e.printStackTrace();
+	        }
+	    }
+	    
+	    public static void main(String[] args) {
+	        launch(args);
+	    }
+	    
 }
