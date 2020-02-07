5c8b85aa81c208d52c38d51459e270a9fb599c7c
zulu-golf <zikkoyaya@gmail.com>
2020-02-07 12:17:05 +0000

    test

--- Smart_Park/src/com/sm/mongo/JavaMongoConnection.java
+++ Smart_Park/src/com/sm/mongo/JavaMongoConnection.java
@@ -9,7 +9,7 @@ public class JavaMongoConnection {
 	public static void main(String args[]) throws Exception{
 		MongoClient mongoClient = new MongoClient(new MongoClientURI("mongodb://localhost:27017"));
 		System.out.println("MongoDB is connected !");
-		//zzz
+		
 		mongoClient.getDatabaseNames().forEach(System.out::println);
 		DB db = mongoClient.getDB("SmartParking");
 		db.getCollectionNames().forEach(System.out::println);
